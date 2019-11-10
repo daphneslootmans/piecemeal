d6eaef99f4daac3205c7de7b096d945a10e15c30
Daphne Slootmans <github@daphneslootmans.be>
2019-11-10 22:38:05 +0100

    Add base app components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <v-app id="app">
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
 </style>
