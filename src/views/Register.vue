7e76b6f860d9376bbef4b7f81c7a1477a061c982
Daphne Slootmans <github@daphneslootmans.be>
2019-11-20 21:01:53 +0100

    Remove unnecessary function

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="columns">
         <div class="column is-6">
-          <form @submit.prevent="registerEmail">
+          <form @submit.prevent="register">
             <div class="content">
               <h2>Register</h2>
             </div>
@@ -23,18 +23,21 @@
             <b-field label="Password">
               <b-input type="password" v-model="password" password-reveal></b-input>
             </b-field>
+            <b-field label="Repeat password">
+              <b-input type="password" v-model="password2" password-reveal></b-input>
+            </b-field>
 
             <div class="level">
               <div class="level-left"></div>
               <div class="level-right">
                 <div class="level-item">
-                  <b-button
-                    type="is-primary"
-                    expanded
-                    @click="registerEmail"
-                  >
-                    Register
-                  </b-button>
+                  <b-button tag="input"
+                            native-type="submit"
+                            value="Register"
+                            expanded
+                            type="is-primary"
+                            @click.prevent="register"
+                  />
                 </div>
               </div>
             </div>
@@ -55,7 +58,7 @@
       return {
         email: '',
         password: '',
-        showPw: false
+        password2: ''
       }
     },
     computed: {
@@ -70,10 +73,15 @@
     methods: {
       ...mapActions({
         registerEmail: 'registerEmail'
-      })
+      }),
+      register () {
+        this.registerEmail({
+          email: this.email,
+          password: this.password
+        })
+      }
     },
     created () {
-      firebase.auth().onAuthStateChanged(user => {this.$store.commit('setAuthUser', user)})
     }
   }
 </script>
