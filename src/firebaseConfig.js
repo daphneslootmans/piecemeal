fe526129d78a8206bc387b9a959af81cb7e78f93
Daphne Slootmans <github@daphneslootmans.be>
2019-12-14 22:21:37 +0100

    remove deprecated firebase settings

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -9,12 +9,6 @@ const db = firebase.firestore()
 const auth = firebase.auth()
 const currentUser = auth.currentUser
 
-// date issue fix according to firebase
-const settings = {
-  timestampsInSnapshots: true
-}
-db.settings(settings)
-
 export {
   db,
   auth,
