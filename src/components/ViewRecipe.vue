18466b41f31d6d5d896e64440322dc0994a45a32
Daphne Slootmans <github@daphneslootmans.be>
2019-12-15 14:39:48 +0100

    Rework view recipe layout

--- src/components/ViewRecipe.vue
+++ src/components/ViewRecipe.vue
@@ -1,30 +1,35 @@
 <template>
   <div class="content">
-    <!--    title-->
-    <div class="level">
-      <div class="level-left">
-        <div class="level-item">
+    <section class="info-section">
+      <!--    title-->
+      <div class="columns mb-0 is-multiline">
+        <div class="column">
           <h1 class="is-marginless">{{ recipe.title }}</h1>
         </div>
-        <div class="level-item">
+        <div class="column is-narrow">
           <b-field label="">
             <b-rate icon="heart" spaced v-model="recipe.rating" disabled/>
           </b-field>
+          <p class="prep-time has-text-right">
+            <vue-fontawesome icon="stopwatch"/>
+            {{ recipe.prepTime }} min
+          </p>
+        </div>
+        <div class="column is-full date-stamp">
+          <p class="is-italic">{{ recipe.createdAt.toDate() | moment('DD-MM-YYYY HH:mm') }}</p>
         </div>
       </div>
-      <div class="level-right">
-        <div class="level-item">
-          <b-button icon-right="pen-alt" type="is-primary" outlined expanded @click="editRecipe(recipe.id)">
-            Edit recipe
-          </b-button>
-        </div>
-      </div>
-    </div>
 
-    <!--    tags-->
-    <div class="level ">
-      <div class="level-left">
-        <div class="level-item">
+      <!--    description-->
+      <div class="columns">
+        <div class="column">
+          <p>{{ recipe.description }}</p>
+        </div>
+      </div>
+
+      <!--    tags-->
+      <div class="columns">
+        <div class="column">
           <div class="tags">
             <div v-for="tag in recipe.tags">
               {{ tag }}
@@ -32,89 +37,75 @@
             <div>{{ recipe.category }}</div>
           </div>
         </div>
-      </div>
-      <div class="level-right">
-        <div class="level-item">
-          <p class="prep-time">
-            <vue-fontawesome icon="stopwatch"/>
-            {{ recipe.prepTime }} min
-          </p>
+        <div class="column is-narrow">
+          <b-button icon-right="pen-alt" type="is-primary" outlined expanded @click="editRecipe(recipe.id)">
+            Edit recipe
+          </b-button>
         </div>
       </div>
-    </div>
+    </section>
 
-    <div class="level">
-      <div class="level-left">
-        <div class="level-item">
-          <p class="is-italic">{{ recipe.description }}</p>
-        </div>
-      </div>
-      <div class="level-right">
-        <div class="level-item">
-          <p>{{ recipe.createdAt.toDate() | moment('DD-MM-YYYY HH:mm') }}</p>
-        </div>
-      </div>
-
-    </div>
-
-    <div class="columns">
-      <div class="column is-half">
-        <div class="card ingredients">
-          <div class="card-header">
-            <div class="card-header-title">
-              Ingredients
+    <section class="body-section">
+      <div class="columns">
+        <div class="column is-half">
+          <div class="card ingredients">
+            <div class="card-header">
+              <div class="card-header-title">
+                Ingredients
+              </div>
             </div>
-          </div>
-          <div class="card-content">
-            <p
-              v-for="ingredient in recipe.ingredients"
-            >
+            <div class="card-content">
+              <p
+                v-for="ingredient in recipe.ingredients"
+              >
                 <span>
                   <vue-fontawesome v-if="randIngredient === ingredient" icon="cookie-bite"/>
                   <vue-fontawesome v-else icon="cookie"/>
                 </span>
-              {{ ingredient }}
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="column is-half" v-if="recipe.materials.length">
-        <div class="card">
-          <div class="card-header">
-            <div class="card-header-title">
-              Materials
+                {{ ingredient }}
+              </p>
             </div>
           </div>
-          <div class="card-content">
-            <p
-              v-for="material in recipe.materials"
-            >
+        </div>
+        <div class="column is-half" v-if="recipe.materials.length">
+          <div class="card">
+            <div class="card-header">
+              <div class="card-header-title">
+                Materials
+              </div>
+            </div>
+            <div class="card-content">
+              <p
+                v-for="material in recipe.materials"
+              >
                 <span>
                   <vue-fontawesome v-if="randMaterial === material" icon="cookie-bite"/>
                   <vue-fontawesome v-else icon="cookie"/>
                 </span>
-              {{ material }}
-            </p>
+                {{ material }}
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div class="columns">
-      <div class="column is-full">
-        <h4>Directions</h4>
-        <div v-for="(step, index) in recipe.directions" class="card">
-          <header class="card-header">
-            <p class="card-header-title">Step {{ index +1 }}</p>
-          </header>
-          <div class="card-content">
-            <p>
-              {{ step }}
-            </p>
+      <div class="columns">
+        <div class="column is-full">
+          <h4>Directions</h4>
+          <div v-for="(step, index) in recipe.directions" class="card">
+            <header class="card-header">
+              <p class="card-header-title">Step {{ index +1 }}</p>
+            </header>
+            <div class="card-content">
+              <p>
+                {{ step }}
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
+
   </div>
 </template>
 
@@ -192,5 +183,18 @@
       margin-right: 0.8em;
       font-size: 0.8em;
     }
+  }
+
+  .detail-row {
+    margin-bottom: 1em;
+  }
+
+  .info-section {
+    margin-bottom: 2rem;
+  }
+
+  .date-stamp {
+    padding-top: 0;
+    font-size: 0.9em;
   }
 </style>
