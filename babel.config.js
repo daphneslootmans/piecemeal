d7fb02d923f58ce98149d6a63cee2d3f0ee371ab
Daphne Slootmans <github@daphneslootmans.be>
2020-05-09 19:28:30 +0200

    Add babel-plugin-transform-remove-console package

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
+let removeConsolePlugin = []
+if (process.env.NODE_ENV === 'production') {
+  removeConsolePlugin = [ ["transform-remove-console", { "exclude": [ "error", "warn"] }] ]
+}
+
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
+    '@vue/app'
+  ],
+  // comment out the line below to allow console.logs in production builds.
+  plugins: removeConsolePlugin
 }
