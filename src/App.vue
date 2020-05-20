73c6fc7585e3231956cc421215ac8fcdf188dc1b
Daphne Slootmans <github@daphneslootmans.be>
2020-05-20 20:34:30 +0200

    Set custom Fonts and change primary color

--- src/App.vue
+++ src/App.vue
@@ -136,10 +136,6 @@
   @import "sass/main.scss";
 
   #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
     min-height: 100vh;
   }
 
